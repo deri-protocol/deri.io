d4b6cf6c73fdb4b7b5bf2515db5dd63e14e77f2c
huangwenchao2000 <wenchao.huang@dfactory.tech>
2022-06-06 17:34:08 +0800

    release app

---
+++ static/js/48.b1ed4d60.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[48],{2056:function(i,e){}}]);
+//# sourceMappingURL=48.b1ed4d60.chunk.js.map
---
+++ static/js/49.6f8bc03e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[49],{2201:function(i,e){}}]);
+//# sourceMappingURL=49.6f8bc03e.chunk.js.map
