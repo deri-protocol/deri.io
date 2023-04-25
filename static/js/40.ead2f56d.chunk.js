a9b811ec1e5ac7441c16067b987a1249deebb900
deri-protocol <deri-protocol@dfactory.tech>
2023-04-25 11:26:35 +0800

    release app

---
+++ static/js/40.ead2f56d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[40],{1720:function(n,i){},2090:function(n,i){},2097:function(n,i){}}]);
+//# sourceMappingURL=40.ead2f56d.chunk.js.map
---
+++ static/js/41.d655bd51.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpderi-client"]=this["webpackJsonpderi-client"]||[]).push([[41],{2309:function(n,i){},2311:function(n,i){},2321:function(n,i){}}]);
+//# sourceMappingURL=41.d655bd51.chunk.js.map
